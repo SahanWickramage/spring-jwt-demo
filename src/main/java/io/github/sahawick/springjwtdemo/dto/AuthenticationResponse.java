25d9e618103e7fb5c5bdae520edd962e9a9545a6
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-06-27 23:58:18 +1000

    added authenication request and token

---
+++ src/main/java/io/github/sahawick/springjwtdemo/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package io.github.sahawick.springjwtdemo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private final String token;
+}
+
